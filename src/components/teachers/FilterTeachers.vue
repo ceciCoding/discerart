6df07799783803a1d53fdcd74ded572f7f43caab
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-19 13:13:20 +0100

    created filter checkboxes

---
+++ src/components/teachers/FilterTeachers.vue
@@ -0,0 +1,18 @@
+<template>
+    <span class="filter-option">
+        <input type="checkbox" id="music" checked>
+        <label for="music">Music</label>
+    </span>
+    <span class="filter-option">
+        <input type="checkbox" id="writting" checked>
+        <label for="writting">Writting</label>
+    </span>
+    <span class="filter-option">
+        <input type="checkbox" id="plastic" checked>
+        <label for="plastic">Plastic Arts</label>
+    </span>
+    <span class="filter-option">
+        <input type="checkbox" id="acting" checked>
+        <label for="acting">Acting</label>
+    </span>
+</template>
--- src/pages/teachers/TeacherDetails.vue
+++ src/pages/teachers/TeacherDetails.vue
@@ -1,12 +1,13 @@
 <template>
     <hero-image id="hero-img"></hero-image>
     <section class="teacher-info">
+        <h1>{{ name }}</h1>
         <div class="img-container">
             <img src="../../assets/t1.jpg" alt="teacher's photo">
         </div>
-        <h1>{{ name }}</h1>
         <h4>{{ fee }}€/hour</h4>
         <p>{{ description }}</p>
+        <h3>Do you need more info? Contact this teacher now!</h3>
         <base-button link :to="contactLink">Contact</base-button>
         <router-view></router-view>
     </section>
@@ -25,6 +26,9 @@ export default {
     computed:{
         contactLink() {
             return this.$route.path + '/' + this.id + '/contact';
+        },
+        name() {
+            return this.selectedTeacher.name;
         },
         area() {
             return this.selectedTeacher.area;
@@ -58,13 +62,34 @@ export default {
     display: none;
 }
 
+.teacher-info {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+}
+
+.teacher-info h4 {
+    align-self: flex-start;
+}
+
+.teacher-info h3 {
+    text-align: center;
+}
+
+.teacher-info p {
+    margin-bottom: 3rem;
+}
+
 @media screen and (min-width: 800px) {
     #hero-img {
         display: block;
     }
 
     .teacher-info {
-        grid-column: 6/12;
+        grid-column: 7/12;
+        width: 100%;
     }
 }
 </style>
