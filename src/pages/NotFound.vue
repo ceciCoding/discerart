c1ddf63535c9cf6e3292ba3ae47d0945be1ae337
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-20 20:24:55 +0100

    404 page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -2,7 +2,37 @@
     <section>
         <h1>Not Found</h1>
         <div class="img-container">
-            <img src="./assets/02.png" alt="page not found drawing">
+            <img src="../assets/02.png" alt="page not found drawing">
         </div>
+        <base-button link to="/">Go back</base-button>
     </section>
 </template>
+
+<style scoped>
+section {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+}
+
+.img-container {
+    width: 100%;
+}
+
+.img-container img {
+    width: 100%;
+}
+
+button {
+    margin-top: 3rem;
+}
+
+@media screen and (min-width: 800px) {
+    section {
+        grid-column: 4/10;
+    }
+}
+</style>
