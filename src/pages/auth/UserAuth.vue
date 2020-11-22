1b9d64d0845613f17e2ac7bc4e98e1327973ad2e
Cecilia Olivera <ceci.coding@gmail.com>
2020-11-22 13:58:22 +0100

    error handling for signup

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="login-hero">
-        <hero-image id="login-hero"></hero-image>
+    <div>
+        <base-modal></base-modal>
+        <div class="login-hero">
+            <hero-image id="login-hero"></hero-image>
+        </div>
+        <form @submit.prevent="submitForm">
+            <h2>discerart</h2>
+            <div class="form-control">
+                <label for="email">Email</label>
+                <input type="text" name="email" v-model.trim="email">
+            </div>
+            <div class="form-control">
+                <label for="email">Password</label>
+                <input type="password" name="password" v-model.trim="password">
+            </div>
+            <span @click="switchMode" v-if="isLogin">Don't have an account? Create one here</span>
+            <span @click="switchMode" v-if="isSignup">Already have an account? Login</span>
+            <p class="error" v-if="!formIsValid">Please enter a valid email and password (at least 6 characters long)</p>
+            <base-button v-if="isLogin">Login</base-button>
+            <base-button v-if="isSignup">Sign Up</base-button>
+        </form>
     </div>
-    <form @submit.prevent="submitForm">
-        <h2>discerart</h2>
-        <div class="form-control">
-            <label for="email">Email</label>
-            <input type="text" name="email" v-model.trim="email">
-        </div>
-        <div class="form-control">
-            <label for="email">Password</label>
-            <input type="password" name="password" v-model.trim="password">
-        </div>
-        <span @click="switchMode" v-if="isLogin">Don't have an account? Create one here</span>
-        <span @click="switchMode" v-if="isSignup">Already have an account? Login</span>
-        <p class="error" v-if="!formIsValid">Please enter a valid email and password (at least 6 characters long)</p>
-        <base-button v-if="isLogin">Login</base-button>
-        <base-button v-if="isSignup">Sign Up</base-button>
-    </form>
 </template>
 
 <script>
@@ -28,26 +31,33 @@ export default {
             password: '',
             formIsValid: true,
             mode: 'login',
-            isLoading: false
+            isLoading: false,
+            error: null
         }
     },
 
     methods: {
-        submitForm() {
+        async submitForm() {
             this.formIsValid = true;
             if (this.email === '' || !this.email.includes('@') || this.password.length < 6) {
                 this.formIsValid = false;
                 return;
             }
-
-            if (this.mode === 'login') {
+            this.isLoading = true;
+            try {
+                if (this.mode === 'login') {
                 //
-            } else {
-                this.$store.dispatch('signup', {
+                } else {
+                await this.$store.dispatch('signup', {
                     email: this.email,
                     password: this.password
-                });
+                    });
+                } 
+            }catch(error) {
+                this.error = error.message || 'Failed to authenticate. Check your login data.';
             }
+            
+            this.isLoading = false;
 
         },
         switchMode(){
